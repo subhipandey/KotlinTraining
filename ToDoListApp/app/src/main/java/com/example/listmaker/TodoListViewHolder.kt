23fcff035bf947f405aafebb43fcaaf0729a0920
Subhi Pandey <subhi2012cpi@gmail.com>
2020-02-12 13:48:26 +0530

    First Commit

---
+++ ToDoListApp/app/src/main/java/com/example/listmaker/TodoListViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.listmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
