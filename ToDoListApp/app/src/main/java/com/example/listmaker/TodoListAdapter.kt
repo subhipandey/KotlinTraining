fcfe56cab332b9691db3b1daf922e829ed79926b
Subhi Pandey <subhi2012cpi@gmail.com>
2020-02-13 13:22:50 +0530

    Binding Data

--- ToDoListApp/app/src/main/java/com/example/listmaker/TodoListAdapter.kt
+++ ToDoListApp/app/src/main/java/com/example/listmaker/TodoListAdapter.kt
@@ -4,15 +4,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
+
+    private val todoLists = arrayOf("Android Developers", "House Work", "Errands", "Shopping")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_list_view_holder, parent, false)
+        return TodoListViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return todoLists.size
     }
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        holder.listPositionTextView.text = (position + 1).toString()
+        holder.listTitleTextView.text = todoLists[position]
+
     }
 }
--- ToDoListApp/app/src/main/java/com/example/listmaker/TodoListViewHolder.kt
+++ ToDoListApp/app/src/main/java/com/example/listmaker/TodoListViewHolder.kt
@@ -1,7 +1,12 @@
 package com.example.listmaker
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+    var listPositionTextView = itemView.findViewById<TextView>(R.id.itemNumber)
+    var listTitleTextView = itemView.findViewById<TextView>(R.id.itemString)
 }
