cb660106065ef8b5473e7bb22e03738d9dc729ac
Subhi Pandey <subhi2012cpi@gmail.com>
2020-02-18 00:26:49 +0530

    Updated UI

--- ToDoListApp/app/src/main/java/com/example/listmaker/MainActivity.kt
+++ ToDoListApp/app/src/main/java/com/example/listmaker/MainActivity.kt
@@ -1,31 +1,34 @@
 package com.example.listmaker
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var todoListRecyclerView: RecyclerView
+    private lateinit var todoListRecyclerView: RecyclerView
+    val listDataManager: ListDataManager = ListDataManager(context: this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val list = listDataManager.readLists()
+
         todoListRecyclerView = findViewById(R.id.list_recyclerview)
         todoListRecyclerView.layoutManager = LinearLayoutManager(context: this)
         todoListRecyclerView.adapter = TodoListAdapter()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener { _ ->
+           showCreateTodoListDialog()
         }
     }
 
@@ -43,5 +46,27 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showCreateTodoListDialog() {
+        val dialogTitle = getString(R.string.name_of_list)
+        var positiveButtonTitle = getString(R.string.create_list)
+        val myDialog = AlertDialog.Builder(context: this)
+        val todoListEditText = EditText(context: this)
+        todoTitleEditText.inputType = InputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
+
+        myDialog.setTitle(dialogTitle)
+        myDialog.setView(todoTitleEditText)
+
+        myDialog.setPositiveButton(positiveButtonTitle){
+            dialog, _  ->
+            val adapter = todoListRecyclerView.adapter as TodoListAdapter
+            val list = TaskList(todoListEditText.text.toString())
+            listDataManager.saveList(list)
+            adapter.addList(list)
+            dialog.dismiss()
+
+        }
+       myDialog.create().show
     }
 }
--- ToDoListApp/app/src/main/java/com/example/listmaker/TodoListAdapter.kt
+++ ToDoListApp/app/src/main/java/com/example/listmaker/TodoListAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
+class TodoListAdapter(val lists: ArrayList<TaskList>) : RecyclerView.Adapter<TodoListViewHolder>() {
 
-    private val todoLists = arrayOf("Android Developers", "House Work", "Errands", "Shopping")
+
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.todo_list_view_holder, parent, false)
@@ -13,13 +16,19 @@ class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return todoLists.size
+        return lists.size
+
     }
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
 
         holder.listPositionTextView.text = (position + 1).toString()
-        holder.listTitleTextView.text = todoLists[position]
+        holder.listTitleTextView.text = lists[position].name
 
+    }
+
+    fun addList(list: TaskList) {
+        lists.add(list)
+        notifyItemInserted(lists.size-1)
     }
 }
