f6cae53bf34a9c07dfde3ea0842aec23a0fb7a12
Subhi Pandey <subhi2012cpi@gmail.com>
2020-03-11 17:09:45 +0530

    TripleAndPair

--- BasicKotlin/src/TripleAndPair.kt
+++ BasicKotlin/src/TripleAndPair.kt
@@ -7,4 +7,21 @@ fun main(args: Array<String>) {
 
     val coordinatesDouble = Pair(2.1, 3.5)
     print("x = ${coordinatesDouble.first} y = ${coordinatesDouble.second}")
+
+    val coordinatesMixed = Pair(2.1,3)
+    print("x = ${coordinatesDouble.first} y = ${coordinatesDouble.second}")
+
+    val x1 = coordinates.first
+    val y1 = coordinates.second
+    print("x1 = ${x1} y1 = ${y1}")
+
+    val coordinates3D = Triple(2,3,1)
+    val (x3, y3 , z3 ) = coordinates3D
+    print("x3 = ${x3} y3 = ${y3} z3 = $z3")
+
+    val (x4, y4, _) = coordinates3D
+    print("x4 = $x4 y4 = $y4")
+
+
+
 }
