93ab2cd1284c5d622c617a3da7c116e95783e150
Subhi Pandey <subhi2012cpi@gmail.com>
2020-03-19 00:00:02 +0530

    Learned about mutable and inmutable state

---
+++ BasicKotlin/src/MutableInMutable.kt
@@ -0,0 +1,15 @@
+fun main (args: Array<String>) {
+    var name = ArrayList<String>()
+    name.add("Sam")
+    name.add("Fred")
+
+    fun PrintNames(names: List<String>) {
+        print(names)
+
+    }
+
+    print(name)
+
+    val myName = "Sam"
+    myName = "Fred"
+}
