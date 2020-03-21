dcf3b3259d05b05698a7ad0b0ff35da8c73db6e2
Subhi Pandey <subhi2012cpi@gmail.com>
2020-03-21 22:56:32 +0530

    Learned about lamdas

---
+++ BasicKotlin/src/Lambdas.kt
@@ -0,0 +1,14 @@
+fun main (args: Array<String>) {
+    val ints = listOf(2 , 3, 20, 50)
+    print(ints.filter {it > 10})
+
+    fun List<Int>.filter(operator: (Int) -> Boolean) {
+
+    }
+
+    val myStringList = listOf("Sam", "Suzzy", "Jesse", "Ray", "Kevin")
+    val updatedList = myStringList.filter { it.length > 3 }.sortedBy { it }
+    print(updatedList)
+
+    view.onClickListner { print("In ClickListner")}
+}
