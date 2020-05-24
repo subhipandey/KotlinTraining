dc285227fc8d326e6bcc966ae99bf3b916fae7f0
Subhi Pandey <subhi2012cpi@gmail.com>
2020-05-24 22:28:45 +0530

    Update ApiService.kt

--- BasicKotlin/SealedClasses/ExampleApp/app/src/main/java/com/subhipandey/android/rickycharacters/data/network/ApiService.kt
+++ BasicKotlin/SealedClasses/ExampleApp/app/src/main/java/com/subhipandey/android/rickycharacters/data/network/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
-  @GET("/api/character/rrr")
+  @GET("/api/character/")
   suspend fun getCharacters(): Response<CharactersResponseModel>
 
 }
