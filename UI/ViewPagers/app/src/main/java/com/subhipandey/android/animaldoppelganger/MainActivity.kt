f9d809b243e95291b85ef2daf4c09c7120bc0be6
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-29 17:19:59 +0530

    Added Toast Message to listen for page changes

--- UI/ViewPagers/app/src/main/java/com/subhipandey/android/animaldoppelganger/MainActivity.kt
+++ UI/ViewPagers/app/src/main/java/com/subhipandey/android/animaldoppelganger/MainActivity.kt
@@ -1,35 +1,47 @@
-
 package com.subhipandey.android.animaldoppelganger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
 
 /**
  * Main Screen
  */
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var doppelgangerNamesArray: Array<String>
+    private lateinit var doppelgangerNamesArray: Array<String>
 
-  //TODO:4 Define page change callback here
+    var doppelgangerPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            Toast.makeText(this@MainActivity, "Selected position: ${position}",
+                    Toast.LENGTH_SHORT).show()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    // Switch to AppTheme for displaying the activity
-    setTheme(R.style.AppTheme)
 
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+        }
+    }
 
-    doppelgangerNamesArray = resources.getStringArray(R.array.doppelganger_names)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Switch to AppTheme for displaying the activity
+        setTheme(R.style.AppTheme)
 
-    val doppelAdapter = DoppelAdapter(this, doppelgangerNamesArray.size)
-    doppelgangerViewPager.adapter = doppelAdapter
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-  }
+        doppelgangerNamesArray = resources.getStringArray(R.array.doppelganger_names)
+
+        val doppelAdapter = DoppelAdapter(this, doppelgangerNamesArray.size)
+        doppelgangerViewPager.adapter = doppelAdapter
+      
+
+        doppelgangerViewPager.registerOnPageChangeCallback(doppelgangerPageChangeCallback)
+
+    }
 
     override fun onDestroy() {
-      super.onDestroy()
+        super.onDestroy()
+        doppelgangerViewPager.unregisterOnPageChangeCallback(doppelgangerPageChangeCallback)
     }
 
 
